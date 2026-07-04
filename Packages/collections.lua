f91e52d6888c9e2abc4ee5073074233a0f47886f
Larsen-dev <GitHubUser12@hotmail.com>
2026-07-04 15:36:22 +0300

    .gitignore update, valid types via wally-package-types.

--- Packages/collections.lua
+++ Packages/collections.lua
@@ -1,7 +1,1 @@
-local collections = require(script.Parent._Index["larsen-dev_collections@0.1.1"]["collections"])
-
-export type set<T> = collections.set<T>
-export type stack<T> = collections.stack<T>
-export type queue<T> = collections.queue<T>
-
-return collections
+return require(script.Parent._Index["larsen-dev_collections@0.1.1"]["collections"])
